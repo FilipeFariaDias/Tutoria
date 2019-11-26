7cdd80d9e8ff09482d83c7cc21fd92a854aceb74
Filipe Faria Dias <35049103+FilipeFariaDias@users.noreply.github.com>
2019-11-26 00:40:58 -0300

    Update TutoriaController.java

--- Tutoria/src/controller/TutoriaController.java
+++ Tutoria/src/controller/TutoriaController.java
@@ -1,8 +1,14 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class TutoriaController {
 
@@ -19,9 +25,68 @@ public class TutoriaController {
     private Tab tabSolicitarReuniao;
 
     @FXML
-    private Tab tabHistoricoReuniao;
+    private TextField textData;
 
     @FXML
-    private Tab tabAgendaReuniao;
+    private TextField textHorario;
+
+    @FXML
+    private TextField textDuracao;
+
+    @FXML
+    private RadioButton radioServicos;
+
+    @FXML
+    private ToggleGroup radioPauta;
+
+    @FXML
+    private RadioButton radioOrientacao;
+
+    @FXML
+    private RadioButton radioMatricula;
+
+    @FXML
+    private RadioButton radioPlanEstudos;
+
+    @FXML
+    private RadioButton radioPlanProjeto;
+
+    @FXML
+    private RadioButton radioAdministrativo;
+
+    @FXML
+    private RadioButton radioAcompanhamento;
+
+    @FXML
+    private RadioButton radioOutros;
+
+    @FXML
+    private Button btnConvidar;
+
+    @FXML
+    private Tab tabHistoricoReuniao;
+    
+    @FXML
+    void apertarConvidar(ActionEvent event) {
+    	String data = textData.getText();
+    	String horario = textHorario.getText();
+    	String duracao = textDuracao.getText();
+    	RadioButton radioTopico = (RadioButton) radioPauta.getSelectedToggle();
+    	
+    	String pauta = radioTopico.getText();
+    	
+    	if(data.isEmpty() || horario.isEmpty() || duracao.isEmpty()) {
+	    	Alert alert = new Alert(Alert.AlertType.INFORMATION);
+	    	alert.setHeaderText(null);
+	    	alert.setContentText("Por favor preencha todos os campos");
+	    	alert.showAndWait();
+	    	return;
+    	}
+    	
+    	System.out.println(data);
+    	System.out.println(horario);
+    	System.out.println(duracao);
+    	System.out.println(pauta);
+    }
 
 }
