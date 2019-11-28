7d7298be0b26b2fe4dde464714c84201032586fd
Filipe Faria Dias <35049103+FilipeFariaDias@users.noreply.github.com>
2019-11-28 14:34:57 -0300

    Update Database.java

--- Tutoria/src/Tutoria/Database.java
+++ Tutoria/src/Tutoria/Database.java
@@ -1,55 +1,52 @@
 package Tutoria;
 
-import java.util.ArrayList;
-import java.util.List;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 
-public class Database {
-	
-	private final List<Reunião> reuniões;
-	private final List<String> topicos;
-	public final List<Usuario> usuario;
-
-	
-	public Database(boolean initdata) {
-		
-		this.reuniões = new ArrayList<Reunião>();
-		this.topicos = new ArrayList<String>();
-		this.usuario = new ArrayList<Usuario>();
-		
-		if(initdata) {
-			initdata();
-		}
-	}
-	
-	
-	private void initdata() {
-		Reunião r1 = new Reunião(1,"Orientação Profissional","25/12/2019","15:00","45 min");
-		Reunião r2 = new Reunião(2,"Matricula","25/12/2019","15:45","20 min");
-		Reunião r3 = new Reunião(3,"Outros","25/12/2019","16:00","40 min");
-		save(r1);
-		save(r2);
-		save(r3);
-		this.topicos.add("Orientação Profissional");
-		this.topicos.add("Matricula");
-		this.topicos.add("Outros");
-		
-		Usuario user1 = new Usuario(1, "Jose", "jose@tutoria.com","jose",123456,"Docente", reuniões, topicos);
-		save(user1);
-		
-	
-	}
-	
-	
-	public List<Usuario> getUsuario() {
-		return usuario;
-	}
+public class ReuniÃ£o {
 
 
-	private void save(Reunião reunião) {
-		this.reuniões.add(reunião);
+	SimpleIntegerProperty id;
+	SimpleStringProperty topico;
+	SimpleStringProperty data;
+	SimpleStringProperty hora;
+	SimpleStringProperty duracao;
+	
+	public int getId() {
+		return id.get();
+	}
+
+	public String getTopico() {
+		return topico.get();
+	}
+
+	public String getData() {
+		return data.get();
+	}
+
+	public String getHora() {
+		return hora.get();
+	}
+
+	public String getDuracao() {
+		return duracao.get();
+	}
+
+	public ReuniÃ£o(Integer id, String topico, String data,
+			String hora, String duracao) {
+		super();
+		this.id = new SimpleIntegerProperty(id);
+		this.topico = new SimpleStringProperty(topico);
+		this.data = new SimpleStringProperty(data);
+		this.hora = new SimpleStringProperty(hora);
+		this.duracao = new SimpleStringProperty(duracao);
 	}
 	
-	private void save(Usuario usuario) {
-		this.usuario.add(usuario);
-	}
+	
+
+
+	
+	
+	
+	
 }
